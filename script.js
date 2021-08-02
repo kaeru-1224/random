2d727a93c9b3de9e6d37980448482e283bd27b65
JISU <cjfslzpf@naver.com>
2021-08-02 16:53:28 +0900

    0802

--- script.js
+++ script.js
@@ -17,24 +17,24 @@ randombtn.addEventListener('click',randomImg)*/
 //든생각: 아마도 객체?를 이용해주면,하나의 묶음으로 넣어줄 수있지 않을까?
 const randombtn= document.querySelector(".random")
 const randomPlanet=[
- { name:"earth",
+ { id:1,
+    name:"earth",
    position:"Galaxy",
    img:"img/01.png",
    detail:"Earth is the third planet from the Sun and the only astronomical object known to harbor and support life. About 29.2% of Earth's surface is land consisting of continents and islands. The remaining 70.8% is covered with water, mostly by oceans, seas, gulfs, and other salt-water bodies, but also by lakes, rivers, and other freshwater, which together constitute the hydrosphere. Much of Earth's polar regions are covered in ice. Earth's outer layer is divided into several rigid tectonic plates that migrate across the surface over many millions of years, while its interior remains active with a solid iron inner core, a liquid outer core that generates Earth's magnetic field, and a convective mantle that drives plate tectonics."
  },
- {
-  name:"Mars",
+ {id:2,
+     name:"Mars",
   position:"Galaxy",
   img:"img/02.png",
   detail:"Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the \"Red Planet\".[16][17] The latter refers to the effect of the iron oxide prevalent on Mars's surface, which gives it a reddish appearance distinctive among the astronomical bodies visible to the naked eye.[18] Mars is a terrestrial planet with a thin atmosphere, with surface features reminiscent of the impact craters of the Moon and the valleys, deserts and polar ice caps of Earth."
 },
-{
+{   id:3,
     name:"moon",
     position:"Galaxy",
     img:"img/03.png",
-    detail:"Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the \"Red Planet\".[16][17] The latter refers to the effect of the iron oxide prevalent on Mars's surface, which gives it a reddish appearance distinctive among the astronomical bodies visible to the naked eye.[18] Mars is a terrestrial planet with a thin atmosphere, with surface features reminiscent of the impact craters of the Moon and the valleys, deserts and polar ice caps of Earth."
-  },
-  {
+    detail:"Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the \"Red Planet\".[16][17] The latter refers to the effect of the iron oxide prevalent on Mars's surface, which gives it a reddish appearance distinctive among the astronomical bodies visible to the naked eye.[18] Mars is a terrestrial planet with a thin atmosphere, with surface features reminiscent of the impact craters of the Moon and the valleys, deserts and polar ice caps of Earth."},
+  { id:4,
     name:"jupiter",
     position:"Galaxy",
     img:"img/04.png",
@@ -44,6 +44,16 @@ const randomPlanet=[
 
   function randomImg (){
     let newImg= randomPlanet[Math.floor(Math.random()* randomPlanet.length)]
- document.querySelector("img").src=newImg.img} 
+    document.querySelector("img").src=newImg.img;
+    document.querySelector(".name").innerText=newImg.name;
+    document.querySelector(".position").innerText=newImg.position;
+    document.querySelector("span").innerText=newImg.detail;} 
+
+
 
  randombtn.addEventListener('click',randomImg)
+
+ //🤔버튼을 누를때 마다  id값이 증가 감소하도록하게해서 바꿔주도록 진행을 해볼까?
+
+
+
